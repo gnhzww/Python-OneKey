06944af1704315085567012740e06c9d9197c142
Moexin <moexin@foxmail.com>
2018-03-19 22:30:49 +0800

    up
    up

---
+++ CentOS-Python3.6.4.sh
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+
+export LANG=UTF-8
+export LANGUAGE=UTF-8
+
+yum groupinstall -y "Development tools"
+yum install -y nano sqlite-devel screen ncurses-devel ncurses-libs zlib-devel mysql-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel
+
+wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz && tar xvJf Python-3.6.4.tar.xz && cd Python-3.6.4
+
+./configure
+make&& make install
+
