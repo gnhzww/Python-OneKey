0e497663e252962b326b5cd1a2ba1412696320d8
Moexin <moexin@foxmail.com>
2018-05-12 21:37:22 +0800

    修复PIP
    修复PIP

--- CentOS-Python3.6.4.sh
+++ CentOS-Python3.6.4.sh
@@ -7,8 +7,12 @@ export LANGUAGE=UTF-8
 yum groupinstall -y "Development tools"
 yum install -y nano sqlite-devel screen ncurses-devel ncurses-libs zlib-devel mysql-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel
 
-wget https://raw.githubusercontent.com/Moexin/Python3.6.4-OneKey/master/Python-3.6.4.tar.xz && tar xvJf Python-3.6.4.tar.xz && cd Python-3.6.4
+wget https://raw.githubusercontent.com/Moexin/Python3.6.4-OneKey/master/Python-3.6.4.tar.xz && tar xvJf Python-3.6.4.tar.xz && mkdir /usr/local/python3 && cd Python-3.6.4
 
-./configure
-make&& make install
+./configure --prefix=/usr/local/python3
+make && make install
 
+mv /usr/bin/python /usr/bin/python_bak
+ln -s /usr/local/python3/bin/python3 /usr/bin/python
+PATH=$PATH:$HOME/bin:
+PATH=$PATH:$HOME/bin:/usr/local/python3/bin
