e3b5e2fa8a328ce1c6e424fd954d7fab127f7973
gnhzww <Franklin4jy95m@gmail.com>
2020-06-12 11:42:55 +0700

    Update and rename Debian-Python3.6.4.sh to Debian-Python.sh

--- Debian-Python.sh
+++ Debian-Python.sh
@@ -7,7 +7,7 @@ export LANGUAGE=UTF-8
 apt-get update -y
 apt-get install -y libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev ca-certificates
 
-wget https://raw.githubusercontent.com/Moexin/Python3.6.4-OneKey/master/Python-3.6.4.tar.xz && tar xvJf Python-3.6.4.tar.xz && cd Python-3.6.4
+wget https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz && tar xvJf Python-3.6.4.tar.xz && cd Python-3.6.4
 
 ./configure
 make&& make install
