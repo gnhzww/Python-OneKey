bfbc8703693ea52d21a354a312c7b972743ee9a5
gnhzww <Franklin4jy95m@gmail.com>
2020-06-12 13:04:26 +0700

    Update CentOS-Python3.6.4.sh

--- CentOS-Python3.6.4.sh
+++ CentOS-Python3.6.4.sh
@@ -7,7 +7,7 @@ export LANGUAGE=UTF-8
 yum groupinstall -y "Development tools"
 yum install -y nano sqlite-devel screen ncurses-devel ncurses-libs zlib-devel mysql-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel
 
-wget https://raw.githubusercontent.com/Moexin/Python3.6.4-OneKey/master/Python-3.6.4.tar.xz && tar xvJf Python-3.6.4.tar.xz && mkdir /usr/local/python3 && cd Python-3.6.4
+wget https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tar.xz && tar xvJf Python-3.8.2.tar.xz && mkdir /usr/local/python3 && cd Python-3.8.2
 
 ./configure --prefix=/usr/local/python3
 make && make install
