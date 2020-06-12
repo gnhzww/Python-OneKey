bf56da068dba27770fded9f5c2b7b1b67b4cb8d2
gnhzww <Franklin4jy95m@gmail.com>
2020-06-12 11:49:35 +0700

    Update Debian-Python.sh

--- Debian-Python.sh
+++ Debian-Python.sh
@@ -7,7 +7,7 @@ export LANGUAGE=UTF-8
 apt-get update -y
 apt-get install -y libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev ca-certificates
 
-wget https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz && tar xvJf Python-3.6.4.tar.xz && cd Python-3.6.4
+wget https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz && tar -zxvf Python-3.8.2.tgz && cd Python-3.8.2
 
 ./configure
 make&& make install
